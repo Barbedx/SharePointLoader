e07f40f621e483661720334ca5ac1dbef2643ae3
Andrii Petruk <andrii.petruk@effem.com>
2020-07-10 09:51:39 +0200

    add excel coverter

---
+++ ExcelConverter/Options.cs
@@ -0,0 +1,66 @@
+﻿using CommandLine;
+
+namespace ExcelConverter
+{
+    partial class Program
+    {
+        class Options
+    {
+        [Option('s', "source", Required = true, HelpText = "Input file to be converted.")]
+        public string InputFile
+        {
+            get;
+            set;
+        }
+
+        [Option('t', "target", Required = true, HelpText = "Target file to output json result.")]
+        public string TargetFile
+        {
+            get;
+            set;
+        }
+
+        [Option('c', "columns", Required = true, HelpText = "How many columns to read.")]
+        public int Columns
+        {
+            get;
+            set;
+        }
+
+        [Option("sheet", Required = true, HelpText = "Name of sheet to read.")]
+        public string SheetName
+        {
+            get;
+            set;
+        }
+
+        [Option("skipRows", Required = false, Default = 0, HelpText = "How many rows to skip.")]
+        public int SkipRows
+        {
+            get;
+            set;
+        }
+
+        [Option('h', "withHeader", Required = false, Default = true, HelpText = "First reading row is a header. If false - set column leteral as header")]
+        public bool WithHeader
+        {
+            get;
+            set;
+        }
+
+        [Option('m', "mainColumn", Required = false, HelpText = "Column where check for existing row, if not specified check first columns(setted by CheckColumns). If this column are empty - close reader and save result")]
+        public int? MainColumn
+        {
+            get;
+            set;
+        }
+
+        [Option("CheckColumns", Required = false, Default = 5, HelpText = "count of first N columns to check for existing row, default first 5 columns. If this columns are empty - close reader and save result")]
+        public int CheckColumns
+        {
+            get;
+            set;
+        }
+    }
+    }
+}
---
+++ ExcelConverter/Program.cs
@@ -0,0 +1,131 @@
+﻿using CommandLine;
+using ExcelDataReader;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelConverter
+{
+    partial class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithNotParsed(HandleParseError)
+                .WithParsed(RunOptions);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> obj)
+        {
+            Console.WriteLine("Please provide correct args!");
+            Environment.Exit(-1);
+        }
+
+        private static void RunOptions(Options options)
+        {
+            //IL_002e: Unknown result type (might be due to invalid IL or missing references)
+            //IL_0034: Expected O, but got Unknown
+            try
+            {
+                using (FileStream fileStream = File.Open(options.InputFile, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(File.Open(options.TargetFile, FileMode.Create), Encoding.Unicode))
+                    {
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(fileStream))
+                        {
+                            using (JsonTextWriter jsonWriter = new JsonTextWriter(streamWriter))
+                            {
+                                while (reader.Name.ToLower() != options.SheetName.ToLower())
+                                {
+                                    reader.NextResult();
+                                }
+                                jsonWriter.Formatting = Formatting.Indented;
+                                for (int i = 0; i < options.SkipRows; i++)
+                                {
+                                    reader.Read();
+                                }
+                                List<string> titles = GetOrCreateHeader(options, reader);
+                                ((JsonWriter)jsonWriter).WriteStartArray();
+                                while (reader.Read())
+                                {
+                                    if (options.MainColumn.HasValue)
+                                    {
+                                        if (string.IsNullOrEmpty(reader.GetString(options.MainColumn.Value)))
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    else if (IsEmptyColumns(options, reader))
+                                    {
+                                        break;
+                                    }
+                                    jsonWriter.WriteStartObject();
+                                    var columnsCount = reader.FieldCount < options.Columns ? reader.FieldCount : options.Columns;
+
+                                    for (int column = 0; column < columnsCount; column++)
+                                    {
+                                        jsonWriter.WritePropertyName(titles[column]);
+                                        jsonWriter.WriteValue((reader)[column] ?? string.Empty);
+                                    }
+                                    jsonWriter.WriteEndObject();
+                                }
+                                jsonWriter.WriteEndArray();
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine($"Sheet {options.SheetName} from file {options.InputFile} parsed succesfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Program filed with message:" + ex.Message);
+                Environment.Exit(-1);
+            }
+        }
+
+        private static List<string> GetOrCreateHeader(Options options, IExcelDataReader reader)
+        {
+            List<string> list = new List<string>();
+            if (options.WithHeader)
+            {
+                reader.Read();
+                var columnsCount = reader.FieldCount < options.Columns ? reader.FieldCount : options.Columns;
+                for (int i = 0; i < columnsCount; i++)
+                {
+                    string title = reader.GetString(i);
+                    title = string.IsNullOrWhiteSpace(title) ? ((char)(65 + i)).ToString() : title;
+                    list.Add(list.Any((string x) => x.Equals(title)) ? (title + $"({(char)(65 + i)})") : title);
+                }
+            }
+            else
+            {
+                for (int j = 0; j < options.Columns; j++)
+                {
+                    list.Add($"{(char)(65 + j)}");
+                }
+            }
+            return list;
+        }
+
+        private static bool IsEmptyColumns(Options options, IExcelDataReader reader)
+        {
+            bool result = true;
+            for (int i = 0; i < options.CheckColumns; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(reader.GetString(i)))
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
